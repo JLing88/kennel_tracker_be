22338062b08ca03f386617f205180285a1d34b1c
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-12-18 13:13:16 -0700

    Add migration for Owners table

---
+++ spec/models/owners_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Owner, type: :model do
+  context 'Relationships' do
+  end
+
+  context 'Validations' do
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
-
 require 'simplecov'
 require 'codecov'
 
