fa57cca043c5decc8f90671f9830b80740bd57c8
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-12-18 13:19:06 -0700

    Add Owner model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_195544) do
+ActiveRecord::Schema.define(version: 2018_12_18_201010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,6 @@ ActiveRecord::Schema.define(version: 2018_12_18_195544) do
     t.string "home_phone"
     t.string "cell_phone"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
