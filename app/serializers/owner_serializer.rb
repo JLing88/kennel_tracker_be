44b309fc82877413ba0a0845743cfdb8cbcc216c
Mike <mikecm@gmail.com>
2018-12-19 21:56:21 -0700

    Add serializers for 3 main resources

---
+++ app/serializers/owner_serializer.rb
@@ -0,0 +1,6 @@
+class OwnerSerializer
+  include FastJsonapi::ObjectSerializer
+  set_key_transform :camel_lower
+
+  attributes :first_name, :last_name, :address, :home_phone, :cell_phone, :email
+end
---
+++ app/serializers/pet_serializer.rb
@@ -0,0 +1,6 @@
+class PetSerializer
+  include FastJsonapi::ObjectSerializer
+  set_key_transform :camel_lower
+
+  attributes :name, :species, :breed, :color, :dob, :spayed_neutered, :medications, :feeding_instructions, :shots
+end
