09aaf4042212de8a31ba89bb1f2ab19b38d1fb48
Mike <mikecm@gmail.com>
2018-12-19 20:29:40 -0700

    Set timezone to match client

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module KennelTrackerBe
   class Application < Rails::Application
+    config.time_zone = "America/New_York"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
