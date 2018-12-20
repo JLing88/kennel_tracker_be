98fa6baca366d1d17212fd5f4d9deab0caea3c25
Mike <mikecm@gmail.com>
2018-12-19 19:57:31 -0700

    Add Current scope to Reservation table

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -4,11 +4,28 @@ RSpec.describe Reservation, type: :model do
   describe 'Relationships' do
     it { should belong_to :pet }
     it { should belong_to :owner }
-   it { should belong_to :run }
   end
 
   describe 'Validations' do
     it { should validate_presence_of(:checkin) }
     it { should validate_presence_of(:checkout) }
   end
+
+  describe 'Class Methods' do
+    context '.current' do
+      it 'returns current reservations' do
+        owner1 = create(:owner)
+        owner2 = create(:owner)
+        reservation1, reservation2 = create_list(:reservation, 2, owner: owner1)
+        reservation3 = create(:reservation, owner: owner2, checkin: Time.current.tomorrow, checkout: Time.current.advance(days: 2))
+
+        result = Reservation.current
+
+        expect(result.first).to be_a(Reservation)
+        expect(result.length).to eq(2)
+        expect(result).to eq([reservation1, reservation2])
+      end
+    end
+  end
 end
+
