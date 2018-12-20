6fad9a8f16af5570bbdd876487c16d20a98e1b31
Mike McKee <mikecm@gmail.com>
2018-12-20 16:10:49 -0700

    Add sad path testing

--- spec/requests/api/v1/owners/create_owners_spec.rb
+++ spec/requests/api/v1/owners/create_owners_spec.rb
@@ -26,5 +26,30 @@ describe 'Owners API Endpoint' do
       expect(result).to have_key(:message)
       expect(result[:message]).to eq('Owner created')
     end
+
+    it 'will not create an owner if any fields are missing' do
+      headers = {
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+      }
+
+      payload = {
+        first_name: 'First',
+        last_name:  '',
+        address:    '',
+        home_phone: '',
+        cell_phone: '(720) 300-5000',
+        email:      'mikecm@gmail.com'
+      }
+
+      post '/api/v1/owners', headers: headers, params: payload.to_json
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+      expect(result).to be_a(Hash)
+      expect(result).to have_key(:error)
+      expect(result[:error]).to eq("Last name can't be blank, Address can't be blank, Home phone can't be blank")
+    end
   end
 end
