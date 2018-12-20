872f0bdaf2e9207e1134fedecfc9b9d2d5f819af
Mike <mikecm@gmail.com>
2018-12-19 20:29:58 -0700

    Refactor migrations for new table column

--- db/migrate/20181218214407_create_reservations.rb
+++ db/migrate/20181218214407_create_reservations.rb
@@ -3,7 +3,7 @@ class CreateReservations < ActiveRecord::Migration[5.2]
     create_table :reservations do |t|
       t.references :pet, foreign_key: true
       t.references :owner, foreign_key: true
-      t.references :run, foreign_key: true
+      t.integer :run_number
       t.datetime :checkin
       t.datetime :checkout
       t.boolean :grooming
--- db/migrate/20181220015952_change_run_id_column_reservations.rb
+++
@@ -1,5 +0,0 @@
-class ChangeRunIdColumnReservations < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :reservations, :run_id, :run_number
-  end
-end
