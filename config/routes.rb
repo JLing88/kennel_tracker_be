b65c512a214e5ad9a9025146e5843d047042f9f4
Mike McKee <mikecm@gmail.com>
2018-12-20 16:11:00 -0700

    Refactor route for creating owner

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/owners', to: 'owners#create'
-
       namespace :reservations do
         get '/current', to: 'current#index'
       end
-      get 'vets', to: 'vets#index'
+
+      post '/owners', to: 'owners#create'
+      get '/vets', to: 'vets#index'
     end
   end
 end
