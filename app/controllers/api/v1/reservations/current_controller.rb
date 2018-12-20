e533961cbb7dc4e1c1f29057656c77e4a84b0bed
Mike <mikecm@gmail.com>
2018-12-19 19:57:31 -0700

    Add Current scope to Reservation table

--- app/controllers/api/v1/reservations/current_controller.rb
+++ app/controllers/api/v1/reservations/current_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Reservations::CurrentController < ApplicationController
   def index
-    render json: { "yay": "yay" }, status: 200
+    render json: Reservation.current, status: 200
   end
 end
