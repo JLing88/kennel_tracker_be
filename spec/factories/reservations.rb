3301eace3a0295fc7aa4d611e9bcfef221b4ac8e
Mike <mikecm@gmail.com>
2018-12-19 19:58:16 -0700

    Change Reservation factory to have better date generation

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :reservation do
-    pet      { create(:pet) }
-    owner    { create(:owner) }
-    run      { create(:run) }
-    checkin  { "2018-12-18 14:44:07" }
-    checkout { "2018-12-18 14:44:07" }
-    grooming { [true, false].sample }
-    daycare  { [true, false].sample }
-    boarding { [true, false].sample }
+    pet        { create(:pet) }
+    owner      { create(:owner) }
+    run_number { rand(1..50) }
+    checkin    { Date.current.beginning_of_day }
+    checkout   { Date.current.end_of_day }
+    grooming   { [true, false].sample }
+    daycare    { [true, false].sample }
+    boarding   { [true, false].sample }
   end
 end
