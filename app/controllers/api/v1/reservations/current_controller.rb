9751d61f7a5dcfa19c70297678e402e8e1e1e629
Mike <mikecm@gmail.com>
2018-12-19 21:56:09 -0700

    Refactor serializer call to set it to be a collection

--- app/controllers/api/v1/reservations/current_controller.rb
+++ app/controllers/api/v1/reservations/current_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Reservations::CurrentController < ApplicationController
   def index
-    render json: Reservation.current, status: 200
+    options = { is_collection: true }
+    render json: ReservationSerializer.new(Reservation.current, options).serialized_json, status: 200
   end
 end
