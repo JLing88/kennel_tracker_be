77f0a7e6cb2de38006ca63d6247a0ab9eaa37dfd
Mike <mikecm@gmail.com>
2018-12-28 14:06:37 -0700

    Create PATCH endpoint for owners

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::OwnersController < ApplicationController
+  before_action :get_owner, :verify_owner, only: :update
+
   def index
     render json: OwnerSerializer.new(Owner.all).serialized_json, status: 200
   end
@@ -13,9 +15,23 @@ class Api::V1::OwnersController < ApplicationController
     end
   end
 
+  def update
+    owner.update(owner_params)
+    render json: { "message": "Owner updated" }, status: 201
+  end
+
   private
+  attr_reader :owner
 
   def owner_params
     params.require(:owner).permit(:first_name, :last_name, :address, :home_phone, :cell_phone, :email)
+  end
+
+  def get_owner
+    @owner ||= Owner.find_by_id(params[:id])
+  end
+
+  def verify_owner
+    render json: { "error": "Owner with ID #{params[:id]} not found" }, status: 400 if owner.nil?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
       get '/owners', to: 'owners#index'
       post '/owners', to: 'owners#create'
+      patch '/owners', to: 'owners#update'
       get '/vets', to: 'vets#index'
       get '/pets', to: 'pets#index'
     end
