6b38df944c461b847629b1b54f2c70f133992ddb
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-12-31 13:37:46 -0700

    Add first_name, last_name, role to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_030925) do
+ActiveRecord::Schema.define(version: 2018_12_31_203035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 2018_12_30_030925) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
+    t.string "first_name"
+    t.string "last_name"
   end
 
   create_table "vets", force: :cascade do |t|
