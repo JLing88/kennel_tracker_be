e3a2bb73bc970d9fcbb5b9673a796c10dee3bbcf
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-01-02 16:56:31 -0700

    Change jwt token exp

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   skip_before_action :authenticate
 
   FOUR_HOURS_LATER = 4*60*60
+  ONE_WEEK_LATER = 604800
 
   def create
     user = User.find_by(email: auth_params[:email])
@@ -9,7 +10,7 @@ class Api::V1::SessionsController < ApplicationController
       render json: { 'error': 'Invalid credentials' }, status: 400
     else
       user.authenticate(auth_params[:password])
-      jwt = Auth.issue({ user: user.id, exp: (Time.now + FOUR_HOURS_LATER).to_i})
+      jwt = Auth.issue({ user: user.id, exp: (Time.now + ONE_WEEK_LATER).to_i})
       render json: {jwt: jwt}
     end
   end
