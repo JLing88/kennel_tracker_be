1038739d39abae6cb13573c4655a9e35e999c820
Mike McKee <mikecm@gmail.com>
2019-01-03 14:49:00 -0700

    Remove auto generated specs for mailer

--- spec/mailers/previews/kennel_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/kennel_mailer
-class KennelMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/kennel_mailer/reservation_confirmation
-  def reservation_confirmation
-    KennelMailerMailer.reservation_confirmation
-  end
-
-end
